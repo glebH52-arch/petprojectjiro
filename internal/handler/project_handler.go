54dc069aaa48127193df702a5957a37a60c70b4d
WangYuLong <glebh52@icloud.com>
2026-07-15 19:01:45 +0800

    fix: finalize project HTTP endpoint

--- internal/handler/project_handler.go
+++ internal/handler/project_handler.go
@@ -21,12 +21,14 @@ func (h *ProjectHandler) CreateProject(w http.ResponseWriter, r *http.Request) {
 	request := createProjectRequest{}
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		http.Error(w, "", http.StatusBadRequest)
+		status := http.StatusBadRequest
+		http.Error(w, http.StatusText(status), status)
 		return
 	}
 	project, err := h.projectService.CreateProject(r.Context(), request.CreatedBy, request.Title, request.Goal)
 	if err != nil {
-		http.Error(w, "", statusFromError(err))
+		status := statusFromError(err)
+		http.Error(w, http.StatusText(status), status)
 		return
 	}
 	response := createProjectResponse{
@@ -40,7 +42,8 @@ func (h *ProjectHandler) CreateProject(w http.ResponseWriter, r *http.Request) {
 	var buf bytes.Buffer
 	err = json.NewEncoder(&buf).Encode(response)
 	if err != nil {
-		http.Error(w, "", statusFromError(err))
+		status := statusFromError(err)
+		http.Error(w, http.StatusText(status), status)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
